f6bc78aedc1838e7fbc0e61372f2ee58bdbd6935
pkumara2 <pkumara2@ford.com>
2019-12-05 08:07:06 +0530

    added HelloWorld program using DotNet

---
+++ JenAppOne/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JenAppOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Designed & Powered By Kumaran P T :) !");        }
+    }
+}
